376c476a1cd75e65572c8556820352141b09adcf
Parrot User <user@localhost.localdomain>
2022-04-21 22:18:55 +0100

    add comparing files and copy of non existent files

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ OBJ = demon.o synchro.o
 program: $(OBJ)
 	gcc $(OBJ) -o program
 $(OBJ): synchro.h
-
+.PHONY: clean
 clean:
 	rm $(OBJ) program
--- synchro.c
+++ synchro.c
@@ -10,14 +10,15 @@
 
 void synchro_pliki(char *source, char *dest); 
 char **create_table(DIR *dir, int *size, char *path);
+char *full_path(char *path, char* name);
 
-// int main(){
-//     synchro_pliki(".", "..");
-// }
+int main(){
+    synchro_pliki(".", "..");
+}
 
 void synchro_pliki(char *source, char *dest){
     DIR *src_dir, *dest_dir; 
-    int src_size, dest_size;
+    int src_size, dest_size, i;
 
     src_size = dest_size = 0; 
     errno = 0;
@@ -28,14 +29,20 @@ void synchro_pliki(char *source, char *dest){
     char **source_paths = create_table(src_dir, &src_size, source);
     char **destination_paths = create_table(dest_dir, &dest_size, dest);
 
-    for (int i=0; i<src_size; i++) {
-        printf("nazwa: %s\n", source_paths[i]);
+    // for (i=0; i<src_size; i++) {
+    //     printf("nazwa: %s\n", source_paths[i]);
+    // }
+    // printf("\n");
+    // for (i=0; i<dest_size; i++) {
+    //     printf("nazwa: %s\n", destination_paths[i]);
+    // }
+
+    for (i = 0; i < src_size; i++) {
+        free(source_paths[i]);
     }
-    printf("\n");
-    for (int i=0; i<dest_size; i++) {
-        printf("nazwa: %s\n", destination_paths[i]);
+    for (i = 0; i < dest_size; i++) {
+        free(destination_paths[i]);
     }
-    
     free(source_paths);
     free(destination_paths);
     closedir(src_dir);
@@ -46,7 +53,7 @@ char **create_table(DIR *dir, int *size, char *path){
     struct dirent *entry;
     struct stat stats;
     int ret;
-    char tmp[0];
+    char *tmp;
 
     int upper_bound = 1;
     char **paths = (char **) malloc(sizeof(char *));
@@ -55,9 +62,7 @@ char **create_table(DIR *dir, int *size, char *path){
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
             continue;
         }
-        strcpy(tmp, path);
-        strcat(tmp, "/");
-        strcat(tmp, entry->d_name);
+        tmp = full_path(path, entry->d_name);
         ret = stat(tmp, &stats);
 
         if (ret) {
@@ -65,12 +70,14 @@ char **create_table(DIR *dir, int *size, char *path){
         }else{
             if ((stats.st_mode & S_IFMT) == S_IFREG) {
                 if (*size < upper_bound) {
-                    paths[(*size)++] = entry->d_name;
+                    paths[(*size)++] = tmp;
                 }else {
                     upper_bound *= 2;
                     paths = (char **) realloc(paths, upper_bound * sizeof(char *));
-                    paths[(*size)++] = entry->d_name;
+                    paths[(*size)++] = tmp;
                 }
+            }else {
+                free(tmp);
             } 
         }
     }
@@ -80,4 +87,28 @@ char **create_table(DIR *dir, int *size, char *path){
     }
 
     return paths;
+}
+
+char *full_path(char *path, char *name){
+    char *tmp;
+    unsigned int path_len, name_len, i, offset;
+
+    path_len = strlen(path);
+    name_len = strlen(name);
+    tmp = (char *) malloc((path_len + name_len + 2) * sizeof(char));
+    offset = 0;
+    
+    for (i = 0; i < path_len; i++){
+        tmp[offset++] = path[i];
+    }
+
+    tmp[offset++] = '/';
+
+    for (i = 0; i < name_len; i++){
+        tmp[offset++] = name[i];
+    }
+
+    tmp[offset] = '\0';
+
+    return tmp;
 }
